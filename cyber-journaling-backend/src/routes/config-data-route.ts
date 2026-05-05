9dafd6a8dba948719d2e026b2188f14ab0671870
Leo <e12519958@student.tuwien.ac.at>
2026-05-05 18:00:07 +0200

    add correct object resolving to get route in config-data-route.ts

--- cyber-journaling-backend/src/routes/config-data-route.ts
+++ cyber-journaling-backend/src/routes/config-data-route.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import prisma from "../db";
 import {ConfigData, Team} from "../../../shared/src/models";
+import {Case, DeviceHealth, EventType, Owner, SeverityLevel} from "shared/dist/models/config-data";
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     const teams:Team[] = await prisma.team.findMany();
-    // TODO
+    const caseState:Case[] = await prisma.case_state.findMany();
+    const deviceHealth:DeviceHealth[] = await prisma.device_health.findMany();
+    const eventType:EventType[] = await prisma.event_type.findMany();
+    const owner:Owner[] = await prisma.owner.findMany();
+    const severityLevel:SeverityLevel[] = await prisma.severity_level.findMany();
     const configData: ConfigData = {
-        caseStateList:[],
-        deviceHealthList:[],
-        eventTypeList:[],
-        ownerList:[],
-        severityLevelList:[],
+        caseStateList:caseState,
+        deviceHealthList: deviceHealth,
+        eventTypeList:eventType,
+        ownerList:owner,
+        severityLevelList:severityLevel,
         teamList: teams
     }
     return res.status(200).json(configData)
