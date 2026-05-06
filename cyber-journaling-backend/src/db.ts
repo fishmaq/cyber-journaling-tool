4b676855e70571fbbe79def0c6225f317d0f76c9
Martin Cerny <martin_cerny@outlook.com>
2026-05-06 08:14:55 +0200

    added helper constants

--- cyber-journaling-backend/src/db.ts
+++ cyber-journaling-backend/src/db.ts
@@ -1,5 +1,5 @@
 import {PrismaClient} from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import {PrismaPg} from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
     connectionString: "postgresql://cyber-journaling:cyber-journaling@localhost:5433/cyber-journaling",
@@ -7,3 +7,41 @@ const adapter = new PrismaPg({
 
 const prisma = new PrismaClient({adapter: adapter})
 export default prisma
+
+// (helper) constants to make including the foreign key references in prisma easier
+export const netplanGroupInclude = {
+    include: {
+        team: true
+    }
+}
+
+export const hostInclude = {
+    include: {
+        netplan: netplanGroupInclude
+    }
+}
+
+export const serviceInclude = {
+    include: {
+        host: hostInclude
+    }
+}
+
+export const journalEventInclude = {
+    include: {
+        severity_level: true,
+        device_health: true,
+        event_type: true,
+        journal_events_services: {
+            include: {
+                service: serviceInclude
+            }
+        }
+    }
+}
+
+export const journalCaseInclude = {
+    include: {
+        journal_event: journalEventInclude
+    }
+}
