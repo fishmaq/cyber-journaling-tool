bf28da9542df35207962720782696beec072fe2c
Leo <e12519958@student.tuwien.ac.at>
2026-05-06 07:52:28 +0200

    add missing s

--- cyber-journaling-backend/src/routes/config-data-route.ts
+++ cyber-journaling-backend/src/routes/config-data-route.ts
@@ -7,17 +7,17 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
     const teams:Team[] = await prisma.team.findMany();
-    const caseState:Case[] = await prisma.case_state.findMany();
-    const deviceHealth:DeviceHealth[] = await prisma.device_health.findMany();
-    const eventType:EventType[] = await prisma.event_type.findMany();
-    const owner:Owner[] = await prisma.owner.findMany();
-    const severityLevel:SeverityLevel[] = await prisma.severity_level.findMany();
+    const caseStates:Case[] = await prisma.case_state.findMany();
+    const deviceHealths:DeviceHealth[] = await prisma.device_health.findMany();
+    const eventTypes:EventType[] = await prisma.event_type.findMany();
+    const owners:Owner[] = await prisma.owner.findMany();
+    const severityLevels:SeverityLevel[] = await prisma.severity_level.findMany();
     const configData: ConfigData = {
-        caseStateList:caseState,
-        deviceHealthList: deviceHealth,
-        eventTypeList:eventType,
-        ownerList:owner,
-        severityLevelList:severityLevel,
+        caseStateList:caseStates,
+        deviceHealthList: deviceHealths,
+        eventTypeList:eventTypes,
+        ownerList:owners,
+        severityLevelList:severityLevels,
         teamList: teams
     }
     return res.status(200).json(configData)
