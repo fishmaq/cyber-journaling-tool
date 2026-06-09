88a90d2dc6b5cf70351431af82433c6fde3b31ab
Martin Cerny <martin_cerny@outlook.com>
2026-06-09 12:47:30 +0200

    update schema.prisma

--- cyber-journaling-backend/src/index.ts
+++ cyber-journaling-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import configDataRoute from './routes/config-data-route';
-import dataRoute from "./routes/data-route";
+import caseRoute from "./routes/case-route";
 
 const PORT = 3001;
 const app = express();
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/configData', configDataRoute)
-app.use('/case', dataRoute)
+app.use('/case', caseRoute)
 
 app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
