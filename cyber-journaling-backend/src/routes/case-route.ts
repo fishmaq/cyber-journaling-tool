639f84758d83c30584580dc3e451fde869328281
Martin Cerny <martin_cerny@outlook.com>
2026-06-09 13:21:01 +0200

    add remaining backend crud funcitonality for case

---
+++ cyber-journaling-backend/src/routes/case-route.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import prisma, {journalCaseInclude} from "../db";
+
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+
+    const journal_cases = await prisma.journal_case.findMany(journalCaseInclude);
+    return res.status(200).json(journal_cases);
+})
+
+router.post("/", async (req, res) => {
+    await prisma.journal_case.create({data: req.body});
+    return res.status(200).send();
+})
+
+router.put("/", async (req, res) => {
+
+    await prisma.journal_case.update({
+        data: removeFkFieldsCase(req.body),
+        where: {
+            id: req.body.id
+        }
+    });
+    return res.status(200).send();
+})
+
+router.delete("/:id", async (req, res) => {
+    await prisma.journal_case.delete({
+        where: {
+            id: Number(req.params.id)
+        }
+    });
+    return res.status(200).send();
+})
+
+function removeFkFieldsCase(data: any) {
+    // remove all unnecessary fk fields, as prisma can't handle them
+    delete data.journal_event;
+    delete data.team;
+    return data;
+}
+
+export default router
