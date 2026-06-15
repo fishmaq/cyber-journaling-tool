f868fc254a3065dab8dddaee5f81f065a0e6d561
Martin Cerny <martin_cerny@outlook.com>
2026-06-15 15:29:00 +0200

    use new routes

--- cyber-journaling-backend/src/index.ts
+++ cyber-journaling-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import configDataRoute from './routes/config-data-route';
 import caseRoute from "./routes/case-route";
+import eventRoute from "./routes/event-route";
+import serviceRoute from "./routes/service-route";
 
 const PORT = 3001;
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.json());
 
 app.use('/configData', configDataRoute)
 app.use('/case', caseRoute)
+app.use('/event', eventRoute)
+app.use('/service', serviceRoute)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
