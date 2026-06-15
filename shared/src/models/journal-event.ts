3aa99a19e5b59178db71279c1a5ec93d6acd8881
Martin Cerny <martin_cerny@outlook.com>
2026-06-15 15:29:19 +0200

    added missing fields for fk relations

--- shared/src/models/journal-event.ts
+++ shared/src/models/journal-event.ts
@@ -4,10 +4,15 @@ import {DeviceHealth, EventType, SeverityLevel} from "./config-data";
 
 export interface JournalEvent extends BasicDbModel {
     severity_level: SeverityLevel,
+    severity_level_id: number | undefined
     device_health: DeviceHealth,
+    device_health_id: number | undefined
     event_type: EventType,
+    event_type_id: number | undefined,
+    case_id: number | undefined,
     timestamp: Date,
     title: string,
     details: string,
     services: Service[],
+    services_ids: number[],
 }
