eb90485cabde129d70c4257488d441b37c04baf5
Martin Cerny <martin_cerny@outlook.com>
2026-06-16 08:16:42 +0200

    added empty button for creating events in the timeline

--- cyber-journaling/src/app/timeline/case-timeline-card/case-timeline-card.ts
+++ cyber-journaling/src/app/timeline/case-timeline-card/case-timeline-card.ts
@@ -1,15 +1,23 @@
 import {Component, input} from '@angular/core';
 import {JournalCase} from 'shared/src/models';
 import {EventTimelineCard} from '../event-timeline-card/event-timeline-card';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'case-timeline-card',
   imports: [
-    EventTimelineCard
+    EventTimelineCard,
+    MatIcon
   ],
   templateUrl: './case-timeline-card.html',
   styleUrl: './case-timeline-card.scss',
 })
 export class CaseTimelineCard {
   journalCase = input.required<JournalCase>();
+
+  createEvent(id: number) {
+    // TODO: implement this
+    // TODO: move this to a service or some kind
+    console.debug('createEvent() %d', id)
+  }
 }
