6e698205cf39bab55d320b627611c2c15936a72f
Martin Cerny <martin_cerny@outlook.com>
2026-07-21 12:56:35 +0200

    Reapply "add filter for closed cases"
    This reverts commit a85403b4f66c23b7ebc370011072db430f280736.

--- cyber-journaling/src/app/timeline/case-timeline/case-timeline.ts
+++ cyber-journaling/src/app/timeline/case-timeline/case-timeline.ts
@@ -1,25 +1,26 @@
-import {Component, effect, inject, OnDestroy, OnInit, signal} from '@angular/core';
-import {JournalCase} from 'shared';
-import {CaseTimelineCard} from '../case-timeline-card/case-timeline-card';
-import {JournalCaseService} from '../../service/journal-case.service';
-import {firstValueFrom} from 'rxjs';
-import {ConfigDataService} from '../../service/config-data.service';
+import { Component, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { JournalCase } from 'shared';
+import { CaseTimelineCard } from '../case-timeline-card/case-timeline-card';
+import { JournalCaseService } from '../../service/journal-case.service';
+import { firstValueFrom } from 'rxjs';
+import { ConfigDataService } from '../../service/config-data.service';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'case-timeline',
-  imports: [
-    CaseTimelineCard,
-  ],
+  imports: [CaseTimelineCard, MatSlideToggle],
   templateUrl: './case-timeline.html',
   styleUrl: './case-timeline.scss',
 })
 export class CaseTimeline implements OnInit, OnDestroy {
   intervalReference = 0;
+  showClosedCase = false;
 
   journalCases = signal<JournalCase[]>([]);
 
-  #journalCaseService = inject(JournalCaseService)
-  #configDataService = inject(ConfigDataService)
+  #journalCaseService = inject(JournalCaseService);
+  #configDataService = inject(ConfigDataService);
+  journalCasesFiltered = this.journalCases();
 
   async ngOnInit() {
     this.intervalReference = setInterval(async () => {
@@ -27,8 +28,13 @@ export class CaseTimeline implements OnInit, OnDestroy {
     }, 10000);
 
     // load data from the service on component init
-    await this.loadData()
+    await this.loadData();
   }
+
+  updateFilteredListEffect = effect(() => {
+    this.journalCases();
+    this.caseVisibilityChanged(this.showClosedCase);
+  });
 
   async refreshForPresenterMode() {
     if (this.#configDataService.presenterMode) {
@@ -37,11 +43,9 @@ export class CaseTimeline implements OnInit, OnDestroy {
   }
 
   async loadData() {
-    const data = await firstValueFrom(
-      this.#journalCaseService.getJournalCases()
-    );
-    console.debug('CaseTimeline: #journalCaseService.getJournalCases():')
-    console.debug(data)
+    const data = await firstValueFrom(this.#journalCaseService.getJournalCases());
+    console.debug('CaseTimeline: #journalCaseService.getJournalCases():');
+    console.debug(data);
 
     this.journalCases.set(data);
     // wait until the list is updated
@@ -54,7 +58,7 @@ export class CaseTimeline implements OnInit, OnDestroy {
     for (const timeline of timelines as HTMLCollectionOf<HTMLElement>) {
       timeline.scrollTo({
         left: timeline.scrollWidth,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     }
   }
@@ -65,14 +69,27 @@ export class CaseTimeline implements OnInit, OnDestroy {
       // wait for a bit to let the dom refresh
       setTimeout(() => {
         this.scrollTimelinesToEnd();
-      }, 500)
+      }, 500);
     }
-  })
+  });
 
   ngOnDestroy(): void {
     // cleanup interval
     if (this.intervalReference) {
       clearInterval(this.intervalReference);
     }
+  }
+
+  caseVisibilityChanged(checked: boolean) {
+    this.showClosedCase = checked;
+    this.journalCasesFiltered = this.journalCases().filter((journalCase) => {
+      if (journalCase.case_state === undefined || this.showClosedCase) {
+        return true;
+      } else {
+        // TODO: maybe refactor
+        return journalCase.case_state.name !== 'Closed';
+      }
+    });
+    console.debug(this.journalCasesFiltered);
   }
 }
